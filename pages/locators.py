885bdbc6aa5b28e1f49b527e087c6a02a48e9325
FalkovAL <Falkov65@mail.ru>
2020-05-04 21:03:14 +0300

    Примеры Независимоть от данных и Независимость контента

--- pages/locators.py
+++ pages/locators.py
@@ -3,7 +3,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    
+    TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-add-to-basket")    
+    PRODUCT_NAME1 = (By.CSS_SELECTOR, ".col-sm-6>h1")    
+    PRODUCT_NAME2 = (By.CSS_SELECTOR, "div.alert:nth-child(1)>div>strong")    
+    PRODUCT_PRICE1 = (By.CSS_SELECTOR, ".col-sm-6>p")    
+    PRODUCT_PRICE2 = (By.CSS_SELECTOR, "div.alert:nth-child(3)>div>p>strong")    
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTR_FORM = (By.CSS_SELECTOR, "#register_form")
